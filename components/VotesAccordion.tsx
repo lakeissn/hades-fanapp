d14bf414a4d64b2711eee29e7681bb5a477f6086
lakeissn <xkrgus9456@naver.com>
2026-02-10 21:09:34 +0900

    Update VotesAccordion.tsx

--- components/VotesAccordion.tsx
+++ components/VotesAccordion.tsx
@@ -1,36 +1,36 @@
-+"use client";
-+
-+import { useState } from "react";
-+import VoteAccordionItem from "./VoteAccordionItem";
-+
-+export type VoteItem = {
-+  id: string;
-+  title: string;
-+  platform: string;
-+  platformLabel: string;
-+  url: string | null;
-+  opensAt?: string;
-+  closesAt?: string;
-+  note?: string;
-+};
-+
-+type VotesAccordionProps = {
-+  votes: VoteItem[];
-+};
-+
-+export default function VotesAccordion({ votes }: VotesAccordionProps) {
-+  const [openId, setOpenId] = useState<string | null>(null);
-+
-+  return (
-+    <div className="votes-accordion">
-+      {votes.map((vote) => (
-+        <VoteAccordionItem
-+          key={vote.id}
-+          vote={vote}
-+          isOpen={openId === vote.id}
-+          onToggle={() => setOpenId(openId === vote.id ? null : vote.id)}
-+        />
-+      ))}
-+    </div>
-+  );
-+}
+"use client";
+
+import { useState } from "react";
+import VoteAccordionItem from "./VoteAccordionItem";
+
+export type VoteItem = {
+  id: string;
+  title: string;
+  platform: string;
+  platformLabel: string;
+  url: string | null;
+  opensAt?: string;
+  closesAt?: string;
+  note?: string;
+};
+
+type VotesAccordionProps = {
+  votes: VoteItem[];
+};
+
+export default function VotesAccordion({ votes }: VotesAccordionProps) {
+  const [openId, setOpenId] = useState<string | null>(null);
+
+  return (
+    <div className="votes-accordion">
+      {votes.map((vote) => (
+        <VoteAccordionItem
+          key={vote.id}
+          vote={vote}
+          isOpen={openId === vote.id}
+          onToggle={() => setOpenId(openId === vote.id ? null : vote.id)}
+        />
+      ))}
+    </div>
+  );
+}
