99f12425bb723c5e6a91ac7737dc0e4020db5e9e
lakeissn <xkrgus9456@naver.com>
2026-02-10 13:35:38 +0900

    Create VotesAccordion.tsx

---
+++ components/VotesAccordion.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import VoteAccordionItem from "./VoteAccordionItem";
+
+export type VoteItem = {
+  id: string;
+  title: string;
+  options?: string[];
+  link?: string;
+  opensAt?: string;
+  closesAt?: string;
+};
+
+type VotesAccordionProps = {
+  votes: VoteItem[];
+};
+
+export default function VotesAccordion({ votes }: VotesAccordionProps) {
+  const [openId, setOpenId] = useState<string | null>(null);
+
+  return (
+    <div className="votes-accordion">
+      {votes.map((vote) => (
+        <VoteAccordionItem
+          key={vote.id}
+          vote={vote}
+          isOpen={openId === vote.id}
+          onToggle={() => setOpenId(openId === vote.id ? null : vote.id)}
+        />
+      ))}
+    </div>
+  );
+}
