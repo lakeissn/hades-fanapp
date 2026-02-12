625aec5a03ab4fb9848f8c9f3009f5bf8112607f
lakeissn <xkrgus9456@naver.com>
2026-02-12 14:58:14 +0900

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,24 +7,12 @@ export default function manifest(): MetadataRoute.Manifest {
     description: "하데스 팬앱",
     start_url: "/",
     display: "standalone",
-    background_color: "#000000",
-    theme_color: "#000000",
+    background_color: "#0a0a0a",
+    theme_color: "#0a0a0a",
     icons: [
-      {
-        src: "/icons/hades_helper.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icons/hades_helper.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-      {
-        src: "/icons/hades_helper.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
+      { src: "/icons/hades_helper.png", sizes: "192x192", type: "image/png" },
+      { src: "/icons/hades_helper.png", sizes: "512x512", type: "image/png" },
+      { src: "/icons/hades_helper.png", sizes: "180x180", type: "image/png" },
     ],
   };
 }
