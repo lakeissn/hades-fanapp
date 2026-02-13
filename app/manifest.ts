39b8e46c11ed8ca126cdb9f038aa6a9f57c53c93
lakeissn <xkrgus9456@naver.com>
2026-02-13 15:14:47 +0900

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,34 +10,36 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#0a0a0a",
     theme_color: "#0a0a0a",
     icons: [
-  {
-    src: "/icon-192",
-    sizes: "192x192",
-    type: "image/png",
-    purpose: "any",
-  },
-  {
-    src: "/icon-512",
-    sizes: "512x512",
-    type: "image/png",
-    purpose: "any",
-  },
-  {
-    src: "/icon-192",
-    sizes: "192x192",
-    type: "image/png",
-    purpose: "maskable",
-  },
-  {
-    src: "/icon-512",
-    sizes: "512x512",
-    type: "image/png",
-    purpose: "maskable",
-  },
-  {
-    src: "/apple-icon.png",
-    sizes: "180x180",
-    type: "image/png",
-    purpose: "any",
-  },
-],
+      {
+        src: "/icon-192",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon-512",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon-192",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/icon-512",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/apple-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "any",
+      },
+    ],
+  };
+}
