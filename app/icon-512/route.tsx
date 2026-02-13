af13d079f75e125970c0e482563ee1f712031f47
lakeissn <xkrgus9456@naver.com>
2026-02-13 15:48:48 +0900

    Update route.tsx

--- app/icon-512/route.tsx
+++ app/icon-512/route.tsx
@@ -1,5 +1,5 @@
 import { createUnifiedIconImage } from "../_iconTemplate";
 
 export async function GET() {
-  return createUnifiedIconImage(570, 570);
+  return createUnifiedIconImage(512, 512);
 }
