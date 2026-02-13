0f8a01d32e241381fc5abf1323bf0eabd0e3c892
lakeissn <xkrgus9456@naver.com>
2026-02-14 03:02:54 +0900

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -19,12 +19,6 @@ export default function manifest(): MetadataRoute.Manifest {
         purpose: "any maskable",
       },
       {
-        src: `/icon-192?v=${androidIconVersion}`,
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "any maskable",
-      },
-      {
         src: `/icon-512?v=${androidIconVersion}`,
         sizes: "512x512",
         type: "image/png",
