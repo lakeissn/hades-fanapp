70e4344b38cd8eedbdd2ef622ddf67b10ba9bac0
lakeissn <xkrgus9456@naver.com>
2026-02-14 01:34:52 +0900

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
-  const androidIconVersion = "android-v6";
+  const androidIconVersion = "android-v7";
 
   return {
     name: "HADES INFO",
