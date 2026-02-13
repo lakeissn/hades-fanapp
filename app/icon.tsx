88fc70a5794ede5f22b198abd88ee76870188dcf
lakeissn <xkrgus9456@naver.com>
2026-02-13 14:43:48 +0900

    Update icon.tsx

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,6 +1,4 @@
-import { ImageResponse } from "next/og";
-import { readFile } from "node:fs/promises";
-import { join } from "node:path";
+import { createUnifiedIconImage } from "./_iconTemplate";
 
 export const size = {
   width: 512,
@@ -10,24 +8,5 @@ export const size = {
 export const contentType = "image/png";
 
 export default async function Icon() {
-  const logo = await readFile(join(process.cwd(), "public", "icons", "hades_helper.png"));
-  const src = `data:image/png;base64,${logo.toString("base64")}`;
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          background: "#0a0a0a",
-        }}
-      >
-        <img src={src} width={338} height={338} alt="Hades helper" />
-      </div>
-    ),
-    size
-  );
+  return createUnifiedIconImage(512, 368);
 }
