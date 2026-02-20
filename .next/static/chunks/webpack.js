6df2b014d67ce19c2512f8ee5cf5e3b7a1e19f01
HUBAGBB <pyosy17@gmail.com>
2026-02-21 02:51:16 +0900

    디자인 롤백

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c56f7948a2f73ea4"; }
+/******/ 		__webpack_require__.h = function() { return "8bd36fe58640287c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
